aae3d78850dad99f7911c71ead9f1819fe1f9b03
Divyanshu Singh <202102133@student.apicollege.edu.au>
2025-04-18 21:52:00 +1000

    Fix: use HashRouter with correct import syntax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter RouterProvider} from 'react-router'
+import { createBrowserRouter, createHashRouter, RouterProvider} from 'react-router'
 import RootLayout from './pages/Layout/RootLayout.js'
 import HomePage from './pages/Home'
 import ProductsPage,{loader as productsLoader} from './pages/Products/Products.js'
